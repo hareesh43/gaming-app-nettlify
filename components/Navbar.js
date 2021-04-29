da33496f22d5c738d822a418dd982d85944ea252
hareesh43 <hareeshgummani@gmail.com>
2021-04-29 20:42:29 +0530

    added login & logout UI

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,24 +4,35 @@ import { useContext } from "react";
 import AuthContext from "../stores/authContext";
 
 export default function Navbar({ children }) {
-  const { user, login } = useContext(AuthContext);
-  console.log(user);
+  const { user, login, logout, authReady } = useContext(AuthContext);
+
   return (
     <div className="container">
       <nav>
         <Image src="/rupee.png" width={50} height={50} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/guides">Guides</Link>
-          </li>
-          <li onClick={login} className="btn">
-            Login/Signup
-          </li>
-        </ul>
+        {authReady && (
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/guides">Guides</Link>
+            </li>
+            {user && <li>{user.email}</li>}
+            {!user && (
+              <li onClick={login} className="btn">
+                Login/Signup
+              </li>
+            )}
+
+            {user && (
+              <li onClick={logout} className="btn">
+                Logout
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={270} />
--- stores/authContext.js
+++ stores/authContext.js
@@ -10,19 +10,47 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [authReady, setAuthReady] = useState(false);
   useEffect(() => {
+    // login event listener
     netlifyIdentity.on("login", (user) => {
       setUser(user);
       netlifyIdentity.close();
       console.log("login event");
     });
-    netlifyIdentity.init();
+
+    // logout event listener
+    netlifyIdentity.on("logout", () => {
+      setUser(null);
+      console.log("logout event");
+    });
+
+       // authReady event listener
+       netlifyIdentity.on("init", (user) => {
+        setAuthReady(true)
+        setUser(user);
+        console.log("init event");
+      });
+
     // initialize netlify identity
-  },[]);
+    netlifyIdentity.init();
+
+    // on DOM unmount remove the event listeners
+
+    return () => {
+      netlifyIdentity.off("login");
+      netlifyIdentity.off("logout");
+    };
+  }, []);
+
   const login = () => {
     netlifyIdentity.open();
   };
-  const context = { user, login };
+
+  const logout = () => {
+    netlifyIdentity.logout();
+  };
+  const context = { user, login, logout,authReady };
   return (
     <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
   );
